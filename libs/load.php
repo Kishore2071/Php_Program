24615d46a039954b17d2e4587ce9974d2f3a1cce
Sibidharan <sibidharan@icloud.com>
2022-01-18 15:46:51 +0000

    web engg day 13

--- libs/load.php
+++ libs/load.php
@@ -13,3 +13,31 @@ function validate_credentials($username, $password)
         return false;
     }
 }
+
+function signup($user, $pass, $email, $phone)
+{
+    $servername = "mysql.selfmade.ninja";
+    $username = "sibidharan";
+    $password = "xyjxo8-xefjat-gYnsif";
+    $dbname = "sibidharan_newdb";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "INSERT INTO `auth` (`username`, `password`, `email`, `phone`, `active`)
+    VALUES ('$user', '$pass', '$email', '$phone', '1');";
+    $error = false;
+    if ($conn->query($sql) === true) {
+        $error = false;
+    } else {
+        // echo "Error: " . $sql . "<br>" . $conn->error;
+        $error = $conn->error;
+    }
+
+    $conn->close();
+    return $error;
+}
