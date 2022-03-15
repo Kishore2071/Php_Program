cde1139e80fef383c498fbc722537a679bc2f110
Sibidharan <sibidharan@icloud.com>
2022-03-15 15:49:21 +0000

    login test

--- libs/includes/User.class.php
+++ libs/includes/User.class.php
@@ -5,7 +5,7 @@ class User
     private $conn;
     public static function signup($user, $pass, $email, $phone)
     {
-        $pass = md5($pass);
+        $pass = md5(strrev(md5($pass))); //Security through obscurity
         $conn = Database::getConnection();
         $sql = "INSERT INTO `auth` (`username`, `password`, `email`, `phone`, `active`)
         VALUES ('$user', '$pass', '$email', '$phone', '1');";
@@ -23,6 +23,20 @@ class User
 
     public static function login($user, $pass)
     {
+        $pass = md5(strrev(md5($pass)));
+        $query = "SELECT * FROM `auth` WHERE `username` = '$user'";
+        $conn = Database::getConnection();
+        $result = $conn->query($query);
+        if ($result->num_rows == 1) {
+            $row = $result->fetch_assoc();
+            if ($row['password'] == $pass) {
+                return $row;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
     }
 
     public function __construct($username)
