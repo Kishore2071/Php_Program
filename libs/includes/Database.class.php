ccbd8ea10eac0305be54d9e0e4466fc3a4fe227d
Sibidharan <sibidharan@icloud.com>
2022-04-04 18:04:06 +0000

    magics

--- libs/includes/Database.class.php
+++ libs/includes/Database.class.php
@@ -6,10 +6,10 @@ class Database
     public static function getConnection()
     {
         if (Database::$conn == null) {
-            $servername = "mysql.selfmade.ninja";
-            $username = "gopikrish";
-            $password = "krishnan";
-            $dbname = "gopikrish_photogram";
+            $servername = get_config('db_server');
+            $username = get_config('db_username');
+            $password = get_config('db_password');
+            $dbname = get_config('db_name');
 
             // Create connection
             $connection = new mysqli($servername, $username, $password, $dbname);
--- libs/includes/test.php
+++
@@ -1,5 +0,0 @@
-<?php
-include "User.class.php";
-
-$obj = new User('gopi');
-echo $obj->getInstagramlink();
